665fc5f5dc23a53dfee7533cd6aabcc2f4e7a959
drocarmo <pe@drocarmo.com>
2014-03-25 17:32:49 -0500

    polishing up the springs, figuring out the screen canvas

--- facebook-paper/chathead/app.js
+++ facebook-paper/chathead/app.js
@@ -1,26 +1,33 @@
 // Configuration
 
 (function() {
-  var boundRadius, boundsView, draggable, position, view, view2;
+  var boundRadius, boundsView, draggable, position, view;
 
   position = {
-    x: 350,
-    y: 770
+    x: 590,
+    y: 350
   };
 
-  boundRadius = 1050;
+  boundRadius = 500;
 
   view = new View({
     x: position.x,
     y: position.y,
-    width: 80,
-    height: 80
+    width: 100,
+    height: 100,
   });
+
+  view.screenFrame = {
+    width: 1136,
+    height: 640
+  }
 
   view.style = {
     borderRadius: "50%",
-    border: "3px solid #fff",
-    background: "url('https://pbs.twimg.com/profile_images/2562282111/7lwoa0jegf7yii69tsjg_bigger.jpeg')"
+    border: "4px solid #fff",
+    boxShadow: "0 1px 10px rgba(0, 0, 0, 0.7)",
+    background: "url('http://learn.shayhowe.com/assets/layout/home/shay.jpg') no-repeat",
+    backgroundSize: "cover"
   };
 
   draggable = new ui.Draggable(view);
@@ -41,7 +48,7 @@
         x: position.x,
         y: position.y
       },
-      curve: "spring(1000,40,1000)"
+      curve: "spring(500,30,1000)"
     });
   });
 
@@ -49,10 +56,6 @@
     width: 772,
     height: 1598
   });
-
-  boundsView.midX = view.midX;
-
-  boundsView.midY = view.midY;
 
   boundsView.style.background = "url('http://f.cl.ly/items/230r2D2K3h2M0F0q1o3E/paper.png') top";
 
